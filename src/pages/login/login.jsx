53313d50cae70e76fd96cbe5d89ae0cf26744d5b
JesusMartinezClavel <jmcvalles@gmail.com>
2024-04-06 22:50:06 +0200

    FEAT -> Created component: custom Button || added it into login view

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,22 +2,40 @@
 import './login.css'
 
 // Methos/modules
+import { useState, useEffect } from "react";
 
 //React components
 import { CInput } from "../../common/c-input/cInput";
+import { CButton } from "../../common/c-button/cButton";
 //Redux
 
 
 export const Login = () => {
+
+    const [loginData, setLoginData] = useState({
+        nickName: "",
+        email: "",
+        birthDate: "",
+        password: ""
+    })
+
+    const inputHandler = (e) => {
+        setLoginData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
     return (
         <div className="loginDesign">
             <CInput
-            className={'CI-LoginDesign'}
+                className={'CI-LoginDesign'}
                 type={"text"}
-                name={"firstName"}
-                value={""}
-                placeholder={"input your first name"}
+                name={"nickName"}
+                value={loginData.nickName || ""}
+                placeholder={"input your nickname"}
             />
+            <CButton title={'button'} />
         </div>
     )
 }
