cfded79fd885fed8ec15cbc06d4da4be2456f5fe
JesusMartinezClavel <jmcvalles@gmail.com>
2024-04-06 22:18:16 +0200

    FEAT -> Created home view

--- src/common/header/header.jsx
+++ src/common/header/header.jsx
@@ -10,8 +10,13 @@ import { Navigator } from "../navigator/navigator";
 export const Header = () => {
     return (
         <div className="headerDesign">
-            <Navigator destination={"/login"} title={"Login"} />
-            <Navigator destination={"/register"} title={"Register"} />
+            <div className="homeHeader">
+                <Navigator destination={"/"} title={"Home"} />
+            </div>
+            <div className="navBarHeader">
+                <Navigator destination={"/login"} title={"Login"} />
+                <Navigator destination={"/register"} title={"Register"} />
+            </div>
         </div>
     )
 }
